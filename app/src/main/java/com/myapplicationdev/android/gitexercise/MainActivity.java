e64886b2a17b6beb9d7d0e33db9ed470f7326cf3
jason-lim <jason_lim@rp.edu.sg>
2020-07-16 11:14:07 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/gitexercise/MainActivity.java
@@ -0,0 +1,19 @@
+package com.myapplicationdev.android.gitexercise;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(MainActivity.this, "This is a Toast", Toast.LENGTH_SHORT);
+
+
+    }
+}
